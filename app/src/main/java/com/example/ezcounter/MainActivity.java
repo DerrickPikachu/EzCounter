02ba87e2f085bd50f4b9d195211ff9008a1dae2c
pikaChin <derrfgh4563@gmail.com>
2019-09-13 18:57:18 +0800

    onLongclick event handle

--- app/src/main/java/com/example/ezcounter/MainActivity.java
+++ app/src/main/java/com/example/ezcounter/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
     private int counter;
     private Button plusOneBtn;
@@ -24,14 +24,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         plusOneBtn = findViewById(R.id.plusOne);
 //        plusOneBtn.setOnClickListener(plusOneListener);
         plusOneBtn.setOnClickListener(this);
+        plusOneBtn.setOnLongClickListener(this);
 
         txv = findViewById(R.id.number);
         txv.setOnClickListener(this);
+        txv.setOnLongClickListener(this);
+    }
+
+    private void plusOne() {
+        counter++;
+        txv.setText(Integer.toString(counter));
+    }
+
+    private void plusTwo() {
+        counter += 2;
+        txv.setText(Integer.toString(counter));
+    }
+
+    private void toZero() {
+        counter = 0;
+        txv.setText(Integer.toString(counter));
     }
 
     @Override
     public void onClick(View view) {
-        counter++;
-        txv.setText(Integer.toString(counter));
+        plusOne();
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        switch(view.getId()) {
+            case R.id.plusOne:
+                plusTwo();
+                break;
+            case R.id.number:
+                toZero();
+        }
+        return true;
     }
 }
