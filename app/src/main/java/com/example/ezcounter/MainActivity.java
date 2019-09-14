e98c6c41bd194c9883105c3b454f2b6bfb686b86
pikaChin <derrfgh4563@gmail.com>
2019-09-14 18:49:16 +0800

    create inner class to be a listener

--- app/src/main/java/com/example/ezcounter/MainActivity.java
+++ app/src/main/java/com/example/ezcounter/MainActivity.java
@@ -7,12 +7,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
-
+public class MainActivity extends AppCompatActivity {
     private int counter;
     private Button plusOneBtn;
     private TextView txv;
-//    private BtnListener plusOneListener;
+
+    private class MainActivityListener implements View.OnClickListener, View.OnLongClickListener{
+
+        private void plusOne() {
+            counter++;
+            txv.setText(Integer.toString(counter));
+        }
+
+        private void plusTwo() {
+            counter += 2;
+            txv.setText(Integer.toString(counter));
+        }
+
+        private void toZero() {
+            counter = 0;
+            txv.setText(Integer.toString(counter));
+        }
+
+        private void minusOne() {
+            counter--;
+            txv.setText(Integer.toString(counter));
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.number:
+                    minusOne();
+                    break;
+                case R.id.plusOne:
+                    plusOne();
+            }
+        }
+        @Override
+        public boolean onLongClick(View view) {
+            switch(view.getId()) {
+                case R.id.plusOne:
+                    plusTwo();
+                    break;
+                case R.id.number:
+                    toZero();
+            }
+            return true;
+        }
+
+    }
+
+    MainActivityListener listener = new MainActivityListener();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,54 +70,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         plusOneBtn = findViewById(R.id.plusOne);
 //        plusOneBtn.setOnClickListener(plusOneListener);
-        plusOneBtn.setOnClickListener(this);
-        plusOneBtn.setOnLongClickListener(this);
+        plusOneBtn.setOnClickListener(listener);
+        plusOneBtn.setOnLongClickListener(listener);
 
         txv = findViewById(R.id.number);
-        txv.setOnClickListener(this);
-        txv.setOnLongClickListener(this);
+        txv.setOnClickListener(listener);
+        txv.setOnLongClickListener(listener);
     }
 
-    private void plusOne() {
-        counter++;
-        txv.setText(Integer.toString(counter));
-    }
-
-    private void plusTwo() {
-        counter += 2;
-        txv.setText(Integer.toString(counter));
-    }
-
-    private void toZero() {
-        counter = 0;
-        txv.setText(Integer.toString(counter));
-    }
-
-    private void minusOne() {
-        counter--;
-        txv.setText(Integer.toString(counter));
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.number:
-                minusOne();
-                break;
-            case R.id.plusOne:
-                plusOne();
-        }
-    }
-
-    @Override
-    public boolean onLongClick(View view) {
-        switch(view.getId()) {
-            case R.id.plusOne:
-                plusTwo();
-                break;
-            case R.id.number:
-                toZero();
-        }
-        return true;
-    }
 }
